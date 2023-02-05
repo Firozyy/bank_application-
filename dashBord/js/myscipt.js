709546c296ccd8cccdd011aa14b57543163095df
unknown <mohammedfiros20@gmail.com>
2023-02-05 10:19:23 +0530

    dash bord clearingl

--- dashBord/js/myscipt.js
+++ dashBord/js/myscipt.js
@@ -6,10 +6,11 @@ let userData;
 //session accesing
 let y = "session"
 let x = localStorage.getItem(y)
-console.log(typeof (x));
+
 ////session end
 
-if (x in localStorage) {
+//data geting from local storage
+if ( y in localStorage) {
     let data_from_localstorage = localStorage.getItem(x);
     userData = JSON.parse(data_from_localstorage)
 }
@@ -17,3 +18,15 @@ console.log(userData);
 let top_name = userData.name
 
 $("#USER_name_top").html(top_name.toUpperCase())
+
+//logout session removing
+function session_remove(){
+    localStorage.removeItem(y);
+}
+//acocount balance 
+
+const balance=userData. balalnce
+$("#account_balance").html(balance)
+//account number
+const accountNnumber=userData.accno
+$("#account_number").html(accountNnumber)
